083ed8c4d4348d32f1f3de27b4a4b52c3d0ca136
Victor Benitez <victorHbenitez85@hotmail.com>
2016-01-13 02:27:20 -0600

    Clase 14 Angularjs controller and  service

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,42 @@
-// Ionic Starter App
+(function (){
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    var app=angular.module('reddit', ['ionic']);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+    app.controller('RedditCtrl', function($http,$scope){
+      
+      $scope.stories = [];
+
+      $http.get('https://www.reddit.com/r/Android/new.json')
+      .success(function(response){
+
+          angular.forEach(response.data.children, function(child){
+
+              $scope.stories.push(child.data);
+              console.log(child.data);
+
+          });
+          
+      });
+       
+
+    });
+
+    app.run(function($ionicPlatform) {
+      $ionicPlatform.ready(function() {
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+         
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+          cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if(window.StatusBar) {
+          StatusBar.styleDefault();
+        }
+      });
+    });
+
+
+
+}());
+
+
